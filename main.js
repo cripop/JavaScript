6e52ddbc778103ed4a67a4665e7ab55d2c4ba0d9
Priyank <kashyappriyankindia@gmail.com>
2021-08-27 19:24:47 +0530

    High order function & Array.
    Learnt about implementation and usage of different high order javascript functions (as a normal function as well as with ES6 funtions).

--- main.js
+++ main.js
@@ -1,28 +1,138 @@
-const myForm = document.querySelector('#my-form');
-const nameInput = document.querySelector('#name');
-const emailInput = document.querySelector('#email');
-const msg = document.querySelector('.msg');
-const userList = document.querySelector('#users');
+const companies = [
+    { name: "Company One", category: "Finance", start: 1981, end: 2003 },
+    { name: "Company Two", category: "Retail", start: 1992, end: 2008 },
+    { name: "Company Three", category: "Auto", start: 1999, end: 2007 },
+    { name: "Company Four", category: "Retail", start: 1989, end: 2010 },
+    { name: "Company Five", category: "Technology", start: 2009, end: 2014 },
+    { name: "Company Six", category: "Finance", start: 1987, end: 2010 },
+    { name: "Company Seven", category: "Auto", start: 1986, end: 1996 },
+    { name: "Company Eight", category: "Technology", start: 2011, end: 2016 },
+    { name: "Company Nine", category: "Retail", start: 1981, end: 1989 }
+];
 
-myForm.addEventListener('submit', onSubmit);
+const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
 
-function onSubmit(e) {
-    e.preventDefault();
+// for (let i = 0; i < companies.length; i++) {
+//     console.log(companies[i]);
+// }
 
-    if (nameInput.value === '' || emailInput.value === '') {
-        msg.classList.add('error');
-        msg.innerHTML = 'Please enter all fields';
+// forEach
 
-        setTimeout(() => msg.remove(), 3000);
-    } else {
-        const li = document.createElement('li');
-        li.appendChild(document.createTextNode(`${nameInput.value} : ${emailInput.value}`));
-        userList.appendChild(li);
+// companies.forEach(function (company) {
+//     console.log(company.name);
+// });
 
-        //Clear fields
-        nameInput.value = '';
-        emailInput.value = '';
 
-    }
-}
+// filter
 
+// Get 21 and older
+
+// let canDrink = [];
+// for (let i = 0; i < ages.length; i++) {
+//     if (ages[i] >= 21) {
+//         canDrink.push(ages[i]);
+//     }
+// }
+
+// const canDrink = ages.filter(function (age) {
+//     if (age >= 21) {
+//         return true;
+//     }
+// });
+
+const canDrink = ages.filter(age => age >= 21);
+
+// Filter retail companies
+
+// const retailCompanies = companies.filter(function (comapny) {
+//     if (comapny.category === 'Retail') {
+//         return true;
+//     }
+// });
+
+const retailCompanies = companies.filter(company => company.category === 'Retail');
+
+// Get 80s companies
+
+const eightiesCompanies = companies.filter(company => (company.start >= 1980 && company.start < 1990));
+
+// Get companies that lasted 10 years or more
+
+const lastedTenYears = companies.filter(company => (company.end - company.start >= 10));
+// map
+
+// Create array of company names
+// const companyNames = companies.map(function (company) {
+//     return company.name;
+// });
+
+// const testMap = companies.map(function (company) {
+//     return `${company.name} [${company.start} - ${company.end}]`;
+// });
+
+const testMap = companies.map((company) => `${company.name} [${company.start} - ${company.end}]`);
+
+
+const ageMap = ages
+    .map(age => Math.sqrt(age))
+    .map(age => age * 2);
+
+
+// sort
+
+
+// Sort companies by start year
+
+// const sortedCompanies = companies.sort(function (c1, c2) {
+//     if (c1.start > c2.start) {
+//         return 1;
+//     } else {
+//         return -1;
+//     }
+// });
+
+// const sortedCompanies = companies.sort((a, b) => a.start > b.start ? 1 : -1);
+
+// Sort ages
+
+// const sortAges = ages.sort((a, b) => a - b);
+
+//console.log(sortAges);
+
+
+
+
+//reduce
+
+// let ageSum = 0;
+// for (let i = 0; i < ages.length; i++) {
+//     ageSum += ages[i];
+// }
+
+
+// const ageSum = ages.reduce(function (total, age) {
+//     return total + age;
+// }, 0);
+
+// const ageSum = ages.reduce((total, age) => total + age, 0);
+
+// Get total year for all companies
+
+// const totalYears = companies.reduce(function (total, comapny) {
+//     return total + (comapny.end - comapny.start);
+// }, 0);
+
+
+// const totalYears = companies.reduce((total, company) => total + (company.end - company.start), 0);
+
+
+
+// Combine Methods
+
+const combined = ages
+    .map(age => age * 2)
+    .filter(age => age >= 40)
+    .sort((a, b) => a - b)
+    .reduce((a, b) => a + b, 0);
+
+console.log(combined);
